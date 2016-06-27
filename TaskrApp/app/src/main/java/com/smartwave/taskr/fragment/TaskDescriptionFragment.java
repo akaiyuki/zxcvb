632cd2ddedff46641a1b682f592ae084d9027b71
Smartwave Dev <smartwavedev@Smartwaves-MacBook-Pro.local>
2016-06-27 12:29:10 +0800

    added floating action button

--- TaskrApp/app/src/main/java/com/smartwave/taskr/fragment/TaskDescriptionFragment.java
+++ TaskrApp/app/src/main/java/com/smartwave/taskr/fragment/TaskDescriptionFragment.java
@@ -1,16 +1,26 @@
 package com.smartwave.taskr.fragment;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.smartwave.taskr.R;
 import com.smartwave.taskr.core.AppController;
 import com.smartwave.taskr.core.BaseActivity;
@@ -27,6 +37,18 @@ import java.util.List;
 public class TaskDescriptionFragment extends Fragment {
 
     private ArrayList<TaskObject> mResultSet = new ArrayList<>();
+
+    private FloatingActionMenu mFloatingButton;
+
+    private FloatingActionButton fab1;
+    private FloatingActionButton fab2;
+    private FloatingActionButton fab3;
+
+
+    private List<FloatingActionMenu> menus = new ArrayList<>();
+    private Handler mUiHandler = new Handler();
+
+    private FloatingActionButton fabEdit;
 
 
     public TaskDescriptionFragment() {
@@ -97,8 +119,82 @@ public class TaskDescriptionFragment extends Fragment {
         mTextProject.setText(SharedPreferencesCore.getSomeStringValue(AppController.getInstance(),"taskproject"));
 
 
+        mFloatingButton = (FloatingActionMenu) view.findViewById(R.id.menu_blue);
+        mFloatingButton.setClosedOnTouchOutside(true);
+        mFloatingButton.hideMenuButton(false);
+
+        fab1 = (FloatingActionButton) view.findViewById(R.id.fab14);
+        fab2 = (FloatingActionButton) view.findViewById(R.id.fab24);
+        fab3 = (FloatingActionButton) view.findViewById(R.id.fab34);
+
+
 
         return view;
     }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        menus.add(mFloatingButton);
+
+        fab1.setOnClickListener(clickListener);
+        fab2.setOnClickListener(clickListener);
+        fab3.setOnClickListener(clickListener);
+
+        int delay = 400;
+        for (final FloatingActionMenu menu : menus) {
+            mUiHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    menu.showMenuButton(true);
+                }
+            }, delay);
+            delay += 150;
+        }
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                fabEdit.show(true);
+//            }
+//        }, delay + 150);
+
+
+
+//        mFloatingButton.setOnMenuButtonClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mFloatingButton.isOpened()) {
+//                    Toast.makeText(getActivity(), mFloatingButton.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
+//                }
+//
+//                mFloatingButton.toggle(true);
+//            }
+//        });
+
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fab14:
+                    Log.d("clicked", "set estimate");
+                    break;
+                case R.id.fab24:
+                    Log.d("clicked", "due date");
+                    break;
+                case R.id.fab34:
+                    Log.d("clicked", "move to finished");
+                    break;
+            }
+        }
+    };
+
+
+
+
 
 }
